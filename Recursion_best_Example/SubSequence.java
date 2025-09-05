dfbaef812d1404334a835c4dc210033d4ada8ded
Saba_Ali_007 <alisabasaifi@gmail.com>
2025-09-05 23:21:32 +0530

    Find the subsequence Time complexity=O(2^n)

---
+++ Recursion_best_Example/SubSequence.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class SubSequence{
+public static void sub(String str,int indx, String newString){
+if(indx==str.length()){
+System.out.println(newString);
+return;
+}
+char current=str.charAt(indx);
+sub(str,indx+1,newString+current);
+sub(str,indx+1,newString);
+}
+
+public static void main(String []args){
+System.out.println("enter the the string");
+Scanner sc=new Scanner(System.in);
+String str=sc.nextLine();
+sub(str,0," ");
+return;
+}
+}
+
+
